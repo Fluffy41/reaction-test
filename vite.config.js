661d249f90055114e0d0578bfa00d350b0809474
Fluffy <flaviofelderer@icloud.com>
2025-03-12 15:00:22 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,4 +8,5 @@ export default defineConfig({
     target: 'esnext', // Ensure compatibility with modern browsers
     outDir: 'dist', // Cloudflare Pages expects the output in the dist directory
   },
-})
+  base: '/', // Set base to root for Cloudflare Pages
+});
