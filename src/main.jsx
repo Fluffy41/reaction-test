73a736274e8525eb84824152e2f44cd547208665
Fluffy <flaviofelderer@icloud.com>
2025-03-12 14:24:49 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ReactionTest from "./App";
+import ReactionTest from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
